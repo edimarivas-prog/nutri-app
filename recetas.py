426595fa2ef7ffafe8f07429f08d951a33122132
edimarivas-prog <edimarivas@gmail.com>
2026-02-09 15:19:32 -0500

    Update recetas.py

--- recetas.py
+++ recetas.py
@@ -1,1 +1,26 @@
 
+# recetas.py
+RECETARIO = {
+    "Desayunos": [
+        {
+            "nombre": "Arepa Fibra-Power",
+            "proteina": 25, # g
+            "ingredientes": [
+                {"item": "Harina PAN", "cantidad_base": 50, "unidad": "g"},
+                {"item": "Salvado de trigo", "cantidad_base": 10, "unidad": "g"},
+                {"item": "Huevos", "cantidad_base": 2, "unidad": "und"}
+            ]
+        },
+        # Aquí agregarás las otras 29...
+    ],
+    "Almuerzos": [
+        {
+            "nombre": "Chaufa de Quinua",
+            "ingredientes": [
+                {"item": "Quinua", "cantidad_base": 80, "unidad": "g"},
+                {"item": "Pollo", "cantidad_base": 180, "unidad": "g"}
+            ]
+        },
+        # Aquí el resto...
+    ]
+}
